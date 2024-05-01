56c10889d8d3b6dca0b1c8356f1bb6f043ec6701
MalshanPerera <malshanperera3@gmail.com>
2024-05-01 23:37:25 +0530

    Fix: Role schema fixed

--- migrations/20240429135704_projects.sql
+++ migrations/20240429135704_projects.sql
@@ -4,23 +4,14 @@ CREATE TABLE IF NOT EXISTS projects(
   id char(21) DEFAULT nanoid() PRIMARY KEY,
   user_id TEXT NOT NULL REFERENCES users(id),
   organization_id TEXT NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
-  name TEXT NOT NULL UNIQUE,
-  key_prefix TEXT NOT NULL UNIQUE,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS projects_organization(
-  id char(21) DEFAULT nanoid() PRIMARY KEY,
-  organization_id TEXT NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
-  project_id TEXT NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  key_prefix TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS projects_users(
   id char(21) DEFAULT nanoid() PRIMARY KEY,
-  organization_id TEXT NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
   project_id TEXT NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
   user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -29,8 +20,7 @@ CREATE TABLE IF NOT EXISTS projects_users(
 
 -- indexes
 CREATE UNIQUE INDEX IF NOT EXISTS projects_key ON projects (organization_id, name, key_prefix);
-CREATE INDEX IF NOT EXISTS projects_user_key ON projects_users (user_id, organization_id, project_id);
-CREATE UNIQUE INDEX IF NOT EXISTS projects_organization_key ON projects_organization (organization_id, project_id);
+CREATE INDEX IF NOT EXISTS projects_user_key ON projects_users (user_id, project_id);
 
 -- trigger functions
 CREATE TRIGGER update_timestamp_trigger_projects
@@ -42,24 +32,16 @@ CREATE TRIGGER update_timestamp_trigger_projects_users
 BEFORE UPDATE ON projects_users
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
-
-CREATE TRIGGER update_timestamp_trigger_projects_organization
-BEFORE UPDATE ON projects_organization
-FOR EACH ROW
-EXECUTE FUNCTION update_timestamp();
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS projects_users;
-DROP TABLE IF EXISTS projects_organization;
 DROP TABLE IF EXISTS projects;
 
 DROP TRIGGER IF EXISTS update_timestamp_trigger_projects_users ON projects_users;
-DROP TRIGGER IF EXISTS update_timestamp_trigger_projects_organization ON projects_organization;
 DROP TRIGGER IF EXISTS update_timestamp_trigger_projects ON projects;
 
 DROP INDEX IF EXISTS projects_user_key;
-DROP INDEX IF EXISTS projects_organization_key;
 DROP INDEX IF EXISTS projects_key;
 -- +goose StatementEnd
--- migrations/20240501094355_roles.sql
+++ migrations/20240501094355_roles.sql
@@ -2,19 +2,11 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS roles(
   id char(21) DEFAULT nanoid() PRIMARY KEY,
-  organization_id char(21) NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
-  default_id char(21) NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
-  name TEXT NOT NULL UNIQUE,
+  organization_id TEXT NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
+  default_id TEXT,
+  name TEXT NOT NULL,
   description TEXT NOT NULL,
   permissions TEXT[] NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS roles_users(
-  id char(21) DEFAULT nanoid() PRIMARY KEY,
-  role_id TEXT NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
-  user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP
 );
@@ -23,6 +15,7 @@ CREATE TABLE IF NOT EXISTS roles_projects(
   id char(21) DEFAULT nanoid() PRIMARY KEY,
   role_id TEXT NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
   project_id TEXT NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+  user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP
 );
@@ -35,11 +28,6 @@ BEFORE UPDATE ON roles
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamp();
 
-CREATE TRIGGER update_timestamp_trigger_roles_users
-BEFORE UPDATE ON roles_users
-FOR EACH ROW
-EXECUTE FUNCTION update_timestamp();
-
 CREATE TRIGGER update_timestamp_trigger_roles_projects
 BEFORE UPDATE ON roles_projects
 FOR EACH ROW
@@ -48,11 +36,9 @@ EXECUTE FUNCTION update_timestamp();
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS roles_users;
 DROP TABLE IF EXISTS roles_projects;
 DROP TABLE IF EXISTS roles;
 
-DROP TRIGGER IF EXISTS update_timestamp_trigger_roles_users ON roles_users;
 DROP TRIGGER IF EXISTS update_timestamp_trigger_roles_projects ON roles_projects;
 DROP TRIGGER IF EXISTS update_timestamp_trigger_roles ON roles;
 -- +goose StatementEnd
