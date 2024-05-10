08e4c9bb40c1676d5c8a0286709d1bf09730dba5
MalshanPerera <malshanperera3@gmail.com>
2024-05-10 20:39:49 +0530

    Fix: Add multiple users

--- testdata/test-users.sql
+++ testdata/test-users.sql
@@ -1,9 +1,6 @@
 INSERT INTO users (id, first_name, last_name, email, password)
-VALUES (
-    '123123',
-    'John',
-    'Maclane',
-    'john@maclane.com',
-    '12312'
-  )
+VALUES 
+    ('123123', 'John', 'Maclane', 'john@maclane.com', '12312'),
+    ('456456', 'Jane', 'Doe', 'jane@doe.com', '45645'),
+    ('789789', 'Alice', 'Smith', 'alice@smith.com', '78978')
 RETURNING *;
